f37de9cca608f910736b3fa94ff25177f2dd94ad
Tazim <tazimmadre5041@gmail.com>
2021-10-02 12:11:36 +0530

    Accordian

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Accordion  from "./components/Accordion";
+import Search  from "./components/Search";
 
 const items=[
   {
@@ -16,5 +17,6 @@ const items=[
   }
 ]
 export default ()=>{
-  return <div><Accordion items={items}/></div>
+  // return <div><Accordion items={items}/></div>
+  return <div><Search/></div>
 };
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React,{useState} from "react";
 
 const Accordion=({items})=>{
-  const renderItems= items.map(item=>{
+  const [activeIndex,setActiveIndex]=useState(null);
+
+  const onTitleClick=(index)=>{
+    setActiveIndex(index);
+  }
+
+  const renderItems= items.map((item,index)=>{
+    const active=index==activeIndex?'active':'';  
     return <React.Fragment key={item.title}>
-    <div className="title active">
-    <i className="dropdown icon"></i>
+    <div className={`title ${active}`} onClick={()=>onTitleClick(index)}>
+    <i className="dropdown icon" ></i>
     {item.title}
     </div>
-    <div className="content active">
+    <div className={`content ${active}`}>
       <p>{item.content}</p>
     </div>
     </React.Fragment> 
