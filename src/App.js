f531dfef6684d5a29802077b33a116a06c9f8646
Tazim <tazimmadre5041@gmail.com>
2021-10-06 21:19:36 +0530

    dropdown added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import React from "react";
-import Accordion  from "./components/Accordion";
-import Search  from "./components/Search";
+import React,{ useState} from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
-const items=[
+const items = [
   {
-    title:'What is React?',
-    content:'React is Frontend JS Framework'
+    title: "What is React?",
+    content: "React is Frontend JS Framework",
   },
   {
-    title:'Why use React?',
-    content:'React is favorite JS library among engineers'
+    title: "Why use React?",
+    content: "React is favorite JS library among engineers",
   },
   {
-    title:'How do you use React?',
-    content:'You use react by creating components'
-  }
-]
-export default ()=>{
+    title: "How do you use React?",
+    content: "You use react by creating components",
+  },
+];
+
+const options = [
+  { label: "The Color Red", value: "red" },
+  { label: "The skyish Blue", value: "blue" },
+  { label: "The Green shade", value: "green" },
+];
+
+export default () => {
+  const [selected,setSelected]= useState(options[0]);
   // return <div><Accordion items={items}/></div>
-  return <div><Search/></div>
+  // return <div><Search/></div>
+  return (
+    <div>
+      <Dropdown 
+      selected={selected}
+      onSelectedChange={setSelected}
+       options={options}/>
+    </div>
+  );
 };
